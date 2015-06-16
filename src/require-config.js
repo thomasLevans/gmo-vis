3c792e084b1fb6d94035f4091986db51204612d9
Tom Evans <tlevans@g.cofc.edu>
2015-06-15 20:44:28 -0400

    initial commit

---
+++ src/require-config.js
@@ -0,0 +1,18 @@
+/*
+*
+*
+*
+*/
+
+'use strict';
+
+require.config({
+  baseUrl:'src',
+  paths: {
+    'jquery':'../node_modules/jquery/dist/jquery.min',
+    'underscore':'../node_modules/underscore/underscore-min',
+    'd3':'../node_modules/d3/d3.min',
+    'app':'app'
+  },
+  deps: ['app']
+});
