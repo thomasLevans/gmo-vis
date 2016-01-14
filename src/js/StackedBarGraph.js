7ad7d609ef9ddbe475d3b9d184289df23a76fa78
Tom Evans <tlevans@Toms-MacBook-Pro.local>
2016-01-13 19:34:36 -0500

    Complete StackedBarGraph constructor, still needs update functionality

--- src/js/StackedBarGraph.js
+++ src/js/StackedBarGraph.js
@@ -12,13 +12,12 @@ var deps = [
 
 define(deps, function(d3) {
 
-  function StackedBarGraph(data, props, svg) {
+  function StackedBarGraph(data, props) {
 
     var graph = this;
 
     graph.data = data || [];
     graph.props = props;
-    graph.svg = svg;
 
     graph.stack = d3.layout.stack();
 
@@ -38,8 +37,48 @@ define(deps, function(d3) {
       .domain([0, graph.yGroupMax])
       .range([props.height, 0]);
 
+    graph.color = d3.scale.linear()
+      .domain([0, n - 1])
+      .range(["#aad", "#556"]);
 
+    graph.xAxis = d3.svg.axis()
+      .scale(x)
+      .tickSize(0)
+      .tickPadding(6)
+      .orient("bottom");
 
+    graph.svg = d3.select(graph.props.container)
+      .append("svg")
+      .attr("width", graph.props.width + graph.props.margin.left + graph.props.margin.right)
+      .attr("height", graph.props.height + graph.props.margin.top + graph.props.margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + props.margin.left + "," + props.margin.top + ")");
+
+    graph.group = svg.selectAll(".group")
+      .data(graph.groups)
+      .enter()
+      .append("g")
+      .attr("class", "group")
+      .style("fill", function(d, i) { return graph.color(i); });
+
+    graph.rect = graph.group.selectAll("rect")
+      .data(function(d) { return d; })
+      .enter()
+      .append("rect")
+      .attr("x", function(d) { return graph.x(d.x); })
+      .attr("y", graph.props.height)
+      .attr("width", graph.x.rangeBand())
+      .attr("height", 0);
+
+    graph.rect.transition()
+      .delay(function(d, i) { return i * 10; })
+      .attr("y", function(d) { return graph.y(d.y0 + d.y); })
+      .attr("height", function(d) { return graph.y(d.y0) - graph.y(d.y0 + d.y); });
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + graph.props.height + ")")
+      .call(graph.xAxis);
   }
 
   StackedBarGraph.prototype.propagateUpdate = function() {
